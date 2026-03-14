03b33b2751a7e0678159b1ee0153669ee5cb2dc1
Janderson <janderson.mcast@gmail.com>
2026-03-14 16:00:39 -0300

    quase Crud

--- sitevendasweb/src/main/java/com/janderson/sitevendasweb/controller/ProdutoController.java
+++ sitevendasweb/src/main/java/com/janderson/sitevendasweb/controller/ProdutoController.java
@@ -1,13 +1,19 @@
 package com.janderson.sitevendasweb.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.janderson.sitevendasweb.entity.Produto;
 import com.janderson.sitevendasweb.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class ProdutoController {
@@ -19,5 +25,42 @@ public class ProdutoController {
     public List<Produto> listarProdutos() {
         return produtoRepository.findAll();
     }
+
+    @PostMapping("/produtos")
+    public Produto salvarProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    @GetMapping("/produtos/{id}")
+    public Produto buscarProdutoPorId(@PathVariable Long id) {
+        Optional<Produto> produto = produtoRepository.findById(id);
+        return produto.orElse(null);
+    }
+    
+    @DeleteMapping("/produtos/{id}")
+    public void deletarProduto(@PathVariable Long id) {
+        produtoRepository.deleteById(id);
+    }
+    
+    @PutMapping("/produtos/{id}")
+    public Produto atualizarProduto(@PathVariable Long id, @RequestBody Produto produtoAtualizado) {
+
+        Optional<Produto> produtoExistente = produtoRepository.findById(id);
+
+        if (produtoExistente.isPresent()) {
+            Produto produto = produtoExistente.get();
+
+            produto.setNome(produtoAtualizado.getNome());
+            produto.setDescricao(produtoAtualizado.getDescricao());
+            produto.setPreco(produtoAtualizado.getPreco());
+            produto.setEstoque(produtoAtualizado.getEstoque());
+            produto.setAtivo(produtoAtualizado.getAtivo());
+
+            return produtoRepository.save(produto);
+        }
+
+        return null;
+    }
+
 
 }
