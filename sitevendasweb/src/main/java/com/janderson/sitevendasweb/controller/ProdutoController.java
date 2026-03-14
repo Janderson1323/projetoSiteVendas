df433ebb63d5039c854ee51a30528bd9960b7800
Janderson <janderson.mcast@gmail.com>
2026-03-14 16:43:41 -0300

    Adicionado paginacao postman

--- sitevendasweb/src/main/java/com/janderson/sitevendasweb/controller/ProdutoController.java
+++ sitevendasweb/src/main/java/com/janderson/sitevendasweb/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.janderson.sitevendasweb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.janderson.sitevendasweb.entity.Produto;
 import com.janderson.sitevendasweb.service.ProdutoService;
-
 
 @RestController
 public class ProdutoController {
@@ -51,5 +51,13 @@ public class ProdutoController {
     public List<Produto> buscarPorNome(@RequestParam String nome) {
         return produtoService.buscarPorNome(nome);
     }
+    
+    @GetMapping("/produtos/paginado")
+    public Page<Produto> listarProdutosPaginados(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
+        return produtoService.listarProdutosPaginados(page, size);
+    }
+
 
 }
--- sitevendasweb/src/main/java/com/janderson/sitevendasweb/service/ProdutoService.java
+++ sitevendasweb/src/main/java/com/janderson/sitevendasweb/service/ProdutoService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.janderson.sitevendasweb.entity.Produto;
 import com.janderson.sitevendasweb.repository.ProdutoRepository;
+
 
 @Service
 public class ProdutoService {
@@ -67,5 +71,11 @@ public class ProdutoService {
     public List<Produto> buscarPorNome(String nome) {
         return produtoRepository.findByNomeContainingIgnoreCase(nome);
     }
+    
+    public Page<Produto> listarProdutosPaginados(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return produtoRepository.findAll(pageable);
+    }
+
 
 }
