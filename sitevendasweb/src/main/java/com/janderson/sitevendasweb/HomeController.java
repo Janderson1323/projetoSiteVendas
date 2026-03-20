73049cf019cb01a8fedf9a27f322c1d1e8249c69
Janderson <janderson.mcast@gmail.com>
2026-03-19 22:26:33 -0300

    feat: adiciona card de produto destaque na página inicial

--- sitevendasweb/src/main/java/com/janderson/sitevendasweb/HomeController.java
+++ sitevendasweb/src/main/java/com/janderson/sitevendasweb/HomeController.java
@@ -1,13 +1,23 @@
 package com.janderson.sitevendasweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.janderson.sitevendasweb.entity.Produto;
+import com.janderson.sitevendasweb.service.ProdutoService;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProdutoService produtoService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+    	Produto produtoDestaque = produtoService.buscarProdutoPorId(1L);
+    	model.addAttribute("produtoDestaque", produtoDestaque);
         return "index";
     }
 }
